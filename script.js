07fb3db11417ea16fb5191849c754bd4d4cfc4cb
RodYam <rodrigo.yoshio.yamamoto@gmail.com>
2023-08-21 15:43:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 async function buscaEndereco(cep) {
     try {
-    var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    var consultaCepConvertida = await consultaCep.json();
-    if (consultaCepConvertida.erro) {
-        throw Error('CEP não existente!');
-    }
-    console.log(consultaCepConvertida);
-    return consultaCepConvertida;
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCepConvertida = await consultaCep.json();
+        if (consultaCepConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        console.log(consultaCepConvertida);
+        return consultaCepConvertida;
     } catch (erro) {
         console.log(erro);
     }
 }
 
-let ceps = ['01001000', '01001001'];
-let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
-Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+var cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscaEndereco(cep.value));
